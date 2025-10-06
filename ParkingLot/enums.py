3b106c924aaa06b21af9c1acbdbae07ab51a9461
Rishabh Agarwal <aggarw266@umn.edu>
2025-10-05 19:48:03 -0500

    added enum & errors

---
+++ ParkingLot/enums.py
@@ -0,0 +1,11 @@
+from enum import Enum, auto
+
+class VehicleType(Enum):
+    BIKE = auto()
+    CAR = auto()
+    TRUCK = auto()
+
+class SpotType(Enum):
+    MOTORBIKE = auto()
+    COMPACT = auto()
+    LARGE = auto()
