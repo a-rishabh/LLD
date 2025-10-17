b72066c4cf6713bf7ac669867ebbf6cf62a5f840
Rishabh Agarwal <aggarw266@umn.edu>
2025-10-16 19:38:35 -0500

    added inventory template

---
+++ VendingMachine/vending_machine/inventory.py
@@ -0,0 +1,13 @@
+
+
+
+class Inventory:
+    """Manages product slots and stock counts."""
+
+    def __init__(self, slots: Dict[str, Slot] | None = None):\
+        pass
+        
+    # ---------------- Admin Ops ---------------- #
+
+    def add_slot(self, product: Product, quantity: int) -> None:
+        pass
