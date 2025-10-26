62018092a913e08121fa46a1d2903524d3d2e669
Rishabh Agarwal <aggarw266@umn.edu>
2025-10-25 22:52:55 -0500

    finished init

--- VendingMachine/vending_machine/machine.py
+++ VendingMachine/vending_machine/machine.py
@@ -16,4 +16,18 @@ class VendingMachine:
         # Payment strategy (default cash)
         self.current_payment: CashPayment = CashPayment(self.drawer)
 
+        # State instances (singletons per machine)
+        self.idle_state = IdleState(self)
+        self.has_money_state = HasMoneyState(self)
+        self.dispense_state = DispenseState(self)
+        self.change_state = ChangeState(self)
+
+        # Initialize state
+        self.state: State = self.idle_state
+
+        # For tracking transaction data
+        self.selected_code: Optional[str] = None
+        self.last_dispensed = None
+        self.last_change: Optional[dict] = None
+
     
