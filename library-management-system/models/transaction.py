9ac95032639210e4a4510d94b551d7e0b262084a
Rishabh Agarwal <aggarw266@umn.edu>
2025-11-04 21:25:35 -0600

    added mark returned

--- library-management-system/models/transaction.py
+++ library-management-system/models/transaction.py
@@ -11,4 +11,5 @@ class BookLending:
         self.due_date = self.issue_date + timedelta(days=BookLending.BORROW_DAYS)
         self.return_date = None
 
-    
+    def mark_returned(self):
+        self.return_date = date.today()
